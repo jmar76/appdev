cb10787e7bc5c727262155e585e7dcfcf762bb1c
jmar76 <jmaguero.ing@gmail.com>
2021-10-10 21:10:39 +0200

    newdeploy

--- db.py
+++ db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('sqlite:///database/tareas.db',
+engine = create_engine('sqlite:///database/tarea.db',
 connect_args={'check_same_thread': False})
 
 Session = sessionmaker(bind=engine)
